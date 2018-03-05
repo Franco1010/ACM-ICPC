49df765f2c4e01393d84def1aa50667d0e71d0f4
Franco1010 <franco.ramirez@alumno.udg.mx>
2018-03-05 16:39:07 -0600

    Create 1000.c

---
+++ COJ/1000.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  1000
+//
+//  Created by Franco Ariel Ramirez Villa on 09/12/16.
+//  Copyright © 2016 Franco Ariel Ramirez Villa. All rights reserved.
+//
+
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%i %i",&a,&b);
+    printf("%i\n",(a+b));
+    return 0;
+}
+
+
+
