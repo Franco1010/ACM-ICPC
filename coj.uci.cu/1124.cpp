b1e5a6e5525c5f34e2afad4b32797c21e0fcf6b0
Franco Ariel Ramirez Villa <franco.ramirez@alumno.udg.mx>
2018-03-11 01:59:11 -0600

    Create 1124.cpp

---
+++ coj.uci.cu/1124.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <stack>
+using namespace std;
+vector<long long int>primos;
+long long l;
+vector<bool> p(1000000,true);
+void criba(){
+    
+    for (int i=2; i<1000000; i++) {
+        if (p[i]) {
+            for (int j=i+i; j<1000000; j+=i) {
+                p[j]=false;
+            }
+        }
+    }
+    for (int i=2; i<1000000; i++) {
+        if (p[i]) {
+            primos.push_back(i);
+        }
+    }
+}
+/*
+bool isPrime(long long int x){
+    if(x==1)return false;
+    long int r= sqrt(x);
+    if (x>1000000) {
+        for (int i=0; i<l; i++) {
+            if (primos[i]>r) {
+                return true;
+            }
+            while (x%primos[i]==0) {
+                return false;
+            }
+        }
+    }else return p[x];
+    return true;
+}*/
+int main(){
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    criba();
+    l=primos.size();
+    long long int c,a,b,i;
+    while (cin>>c, c!=0) {
+        for (i=2; i<c/2+1; i++) {
+            if (p[i] && p[c-i]) {
+                cout<<c<<" = "<<i<<" + "<<c-i<<'\n';
+                break;
+            }
+        }
+    }
+    return 0;
+}
+
+//transform (foo, foo+4, bar, result, std::logical_and<bool>());
