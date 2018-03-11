6660000e6bdadf835cf9258070725b5189fc4caa
Franco Ariel Ramirez Villa <franco.ramirez@alumno.udg.mx>
2018-03-11 01:59:53 -0600

    Create 2427.cpp

---
+++ coj.uci.cu/2427.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <stack>
+using namespace std;
+long long l;
+vector<bool> p(1000000,true);
+vector<long long int> primos;
+void criba(){
+    p[0]=false;p[1]=false;
+    long long int cont=0;
+    for (int i=2; i<1000000; i++) {
+        if (p[i]) {
+            for (int j=i+i; j<1000000; j+=i) {
+                p[j]=false;
+            }
+        }
+    }
+    for (int i=0; i<1000000; i++) {
+        if (p[i]) {
+            primos.push_back(i);
+        }
+        //pp.push_back(cont);
+    }
+}
+/*
+bool isPrime(long long int x){
+    if(x==1)return false;
+    long int r= sqrt(x);
+    if (x>1000000) {
+        for (int i=0; i<l; i++) {
+            if (primos[i]>r) {
+                return true;
+            }
+            while (x%primos[i]==0) {
+                return false;
+            }
+        }
+    }else return p[x];
+    return true;
+}*/
+int main(){
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    criba();
+    vector<long long int>::iterator l1,l2;
+    long long int a,b,h,g;
+    while (cin>>a>>b) {
+        if(a==0 && b==0)break;
+        l1=lower_bound(primos.begin(), primos.end(), a);
+        l2=upper_bound(primos.begin(), primos.end(), b);
+        h=l2-primos.begin();
+        g=l1-primos.begin();
+        if(primos[h]==b) h++;
+        cout<<h-g<<'\n';
+    }
+    return 0;
+}
+
+//transform (foo, foo+4, bar, result, std::logical_and<bool>());
