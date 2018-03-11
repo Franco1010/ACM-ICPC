279d2523acb511a601085203cb1e485a8f468417
Franco Ariel Ramirez Villa <franco.ramirez@alumno.udg.mx>
2018-03-11 01:54:14 -0600

    Create 1306.py

---
+++ coj.uci.cu/1306.py
@@ -0,0 +1,11 @@
+lista=[]
+Num=int(raw_input())
+for b in range(Num):
+    a=int(raw_input())
+    lista.append(a)
+for i in lista:
+    b=i%4
+    if b==0:
+        print('YES')
+    else:
+        print('NO')
