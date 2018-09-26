6d236e30f673568ebd13233c5ee29c86c4cc86f9
Franco Ariel Ramirez Villa <franco.ramirez@alumno.udg.mx>
2018-09-26 09:48:40 -0500

    Create 2637.cpp

---
+++ COJ/2637.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PI 3.14159265358979323846
+#define EPS 1e-6
+#define INF 1e5+1
+#define MOD 1000000007
+
+#define _ ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0), cout.precision(15);
+#define forr(i,a,b) for(int i=(a); i<(b); i++)
+#define forn(i,n) forr(i,0,n)
+#define sz(c) ((int)c.size())
+#define zero(v) memset(v, 0, sizeof(v))
+#define forall(it,v) for(auto it=v.begin();it!=v.end();++it)
+#define pb push_back
+#define fst first
+#define snd second
+#define dforn(i,n) for(int i=n-1; i>=0; i--)
+#define dprint(v) cout << #v"=" << v << endl
+#define ENDL '\n'
+
+#define toIntA(c) (c - 'A')
+#define toInta(c) (c - 'a')
+#define toInt0(c) (c - '0')
+#define toCharA(i) (char)(i + 'A')
+#define toChara(i) (char)(i + 'a')
+#define toChar0(i) (char)(i + '0')
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+
+template<typename T>
+class fenwick{
+public:
+  vector<T> fenw;
+  int n;
+  fenwick(int _n){
+    n = _n;
+    fenw.resize(n + 1, {});
+  }
+
+  void update(int x, T val){
+    while( x <= n ){
+      fenw[x] += val;
+      x += x & -x;
+    }
+  }
+
+  T query(int x){
+    T val = {};
+    while( x > 0 ){
+      val += fenw[x];
+      x -= x & -x;
+    }
+  }
+};
+
+
+int main() { _
+  ll t,n,q,aux,l,r;
+  cin>>t;
+  while(t--){
+    cin>>n>>q;
+    //fenwick<ll>f(n);
+    vector<ll>v(n+1);
+    v[0]=0;
+    forn(i,n){
+      cin>>v[i+1];
+      v[i+1]+=v[i];
+    }
+    while(q--){
+      cin>>l>>r;
+      cout<<v[r+1]-v[l]<<ENDL;
+    }if(t)cout<<ENDL;
+  }
+  return 0;
+}
