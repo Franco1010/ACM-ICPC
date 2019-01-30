f3a9915a3de40eb8b4daee210692a72a1fd1f56a
abrahamfourteen <36650502+abrahamfourteen@users.noreply.github.com>
2019-01-29 22:05:38 -0600

    Update 100650A - ACM (ACronym Maker).cpp

--- Codeforces/100650A - ACM (ACronym Maker).cpp
+++ Codeforces/100650A - ACM (ACronym Maker).cpp
@@ -22,63 +22,32 @@ typedef unsigned long long ulli;
 typedef pair<int, int> ii;
 
 const int N = 155;
-const int inf = 1e9 + 9;
 
+// {ith letter of abbreviation, jth significant word, k letters of significant[j] added}
 int dp[N][N][N];
 int done[N][N][N];
 int timer = 0;
-set<string> insignificant;
+set<string> insignificants;
+vector<string> significants;
 string abbreviation;
-string significant;
-int who[N];
-string word;
+string tmp;
 int n;
 
-void divide(){
-  significant = "";
-  abbreviation = "";
-  stringstream ss;
-  string tmp;
-  ss << word;
-  set<int> marks;
-  while( ss >> tmp ){
-    if( !insignificant.count(tmp) ){
-      if( abbreviation.empty() ){
-        abbreviation = tmp;
-      }else{
-        significant += tmp;
-        marks.insert(size(significant));
-      }
-    }
-  }
-  who[0] = 0;
-  For1(i, size(significant)){
-    who[i] = who[i - 1];
-    if( marks.count(i) ){
-      who[i]++;
-    }
-  }
-}
-
-// {i-th of abbreviation, j-th significant words, using k-th significant words}
 int calc(int i, int j, int k){
-  //printf("i: %d, j: %d, k: %d\n", i, j, k);
   if( i == size(abbreviation) ){
-    return k == who[size(significant) - 1];
+    return j == size(significants);
   }
-  if( j >= size(significant) ){
-    return 0;
-  }
-  if(k >= who[size(significant) - 1]){
+  if( j == size(significants) || k == size(significants[j]) ){
     return 0;
   }
   int &ans = dp[i][j][k];
   if( done[i][j][k] != timer ){
     done[i][j][k] = timer;
     ans = 0;
-    For(nj, j, size(significant), +1){
-      if( tolower(abbreviation[i]) == significant[j] ){
-        ans += calc(i + 1, nj + 1, k + (who[nj] != who[j-1]));
+    For(nk, k, size(significants[j]) - 1, +1){
+      if( tolower(abbreviation[i]) == significants[j][nk] ){
+        ans += calc(i + 1, j + 1, 0); // Next word
+        ans += calc(i + 1, j, nk + 1); // Same word
       }
     }
   }
@@ -87,29 +56,28 @@ int calc(int i, int j, int k){
 
 int main(){ _
   while( cin >> n, n != 0 ){
-    insignificant = set<string>();
+    insignificants = set<string>();
     Forn(i, n){
-      string s;
-      cin >> s;
-      insignificant.insert(s);
+      cin >> tmp;
+      insignificants.insert(tmp);
     }
-    getline(cin, word);
-    while( getline(cin, word) && word != "LAST CASE" ){
-      divide();
-      debug(abbreviation);
-      debug(significant);
-      Forn(i, size(significant)){
-        cout << who[i] << " ";
+    getline(cin, tmp);
+    while( getline(cin, tmp) && tmp != "LAST CASE" ){
+      stringstream ss;
+      ss << tmp;
+      ss >> abbreviation;
+      significants = vector<string>();
+      while( ss >> tmp ){
+        if( !insignificants.count(tmp) ){
+          significants.pb(tmp);
+        }
       }
-      cout << '\n';
-
       ++timer;
       if( calc(0, 0, 0) == 0 ){
         cout << abbreviation << " is not a valid abbreviation\n";
       }else{
         cout << abbreviation << " can be formed in " << calc(0, 0, 0) << " ways\n";
       }
-      puts("");
     }
   }
   return 0;
